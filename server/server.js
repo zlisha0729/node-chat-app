4432dfe199d00032ed2a79d2ab958be6ab45bb0c
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-07 13:52:44 -0400

    Init commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+
+var app = express();
+
+console.log(publicPath);
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+})
