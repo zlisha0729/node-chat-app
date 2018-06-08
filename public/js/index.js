47a6e971026f26fc1615875e8d3ff94d49c7c2d9
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-07 20:42:48 -0400

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,27 @@ socket.on('disconnect', function() {
 });
 
 
-socket.on('newMessage', function(message) {
-  console.log('New message', message);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//   from: "George",
+//   text: "Nice to meet you"
+// }, function(data) {
+//   console.log('got it', data);
+// })
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  })
 })
--- server/server.js
+++ server/server.js
@@ -29,9 +29,10 @@ io.on('connection', (socket) => {
   //   createdAt: 123
   // })
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
     // send message to everybody but myself
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
